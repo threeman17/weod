92ecf6a427d9223dbe8eaacdb5705fa259b73889
cewin <cewin95@foxmail.com>
2017-09-13 18:36:47 +0800

    修改壁纸

--- weod/src/image/controller/BasicController.java
+++ weod/src/image/controller/BasicController.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BasicController {
 
+	/**
+	 * 通过参数获得用户的值
+	 * @author cewin
+	 * @email cewin95@foxmaill.com
+	 * @version 1.0
+	 * @date 2017年9月13日	下午6:33:28
+	 * @param username
+	 * @return
+	 */
 	@RequestMapping("/weodimage.action")
 	public String getUserInfo(String username) {
 		System.out.println(username);
-		return "index";
+		return "static/weodimage";
 		
 	}
 	
