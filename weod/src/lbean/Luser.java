66a45f3a0d4613ab90cc78f31c379d07900ab088
yoki <1293193511@qq.com>
2017-09-13 23:21:56 +0800

    更换菜单图标，添加DBhelper，创建了用户的类

---
+++ weod/src/lbean/Luser.java
@@ -0,0 +1,21 @@
+package lbean;
+
+import java.util.Date;
+
+public class Luser {
+
+		private String luserid;
+
+		private String lpassword;
+
+		private String lemail;
+
+		private Date lcreatetime;
+
+		private Date lupdatetime;
+
+		private String lheadpic;
+
+		private String lnickname;
+		 
+}
